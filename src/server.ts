19eea18f157c882c33b86ec2a154439b28c456bd
Reginald Osano <rgo02439@gmail.com>
2024-07-11 21:40:05 +0800

    refactor codes and cleanup

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,6 @@ import http from "http";
 import App from "@src/express/appService";
 import dotenv from "dotenv";
 import { SERVER_RUNNING_MESSAGE } from "@src/messages/constants";
-import { sendMail } from "@src/services/email";
-import {
-  SUBJECT_CONTENT,
-  TEXT_CONTENT,
-  HTML_CONTENT,
-} from "@src/services/email/content";
 
 dotenv.config();
 
@@ -28,15 +22,6 @@ const StartServer = async (server_status_message: string) => {
 };
 
 const runningServer = StartServer(SERVER_RUNNING_MESSAGE);
-
-const emailVerifyOTPBody = {
-  emailSentTo: "regie30developer@gmail.com",
-  emailSubject: SUBJECT_CONTENT,
-  emailText: TEXT_CONTENT,
-  emailHTML: HTML_CONTENT,
-};
-
-sendMail(emailVerifyOTPBody);
 
 runningServer
   .then(() => {
